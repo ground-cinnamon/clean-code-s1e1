6ac062a8943943f6e480c2d6bf505e761902beae
ground-cinnamon <morkovog@gmail.com>
2024-01-08 13:02:54 +0300

    refactor: update CSS, HTML, JS according to BEM rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -34,20 +34,20 @@ var createNewTaskElement=function(taskString){
 
     listItem.classList.add("task-item");
     label.innerText=taskString;
-    label.className='task-title';
+    label.className='task-item__title';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
-    checkBox.classList.add("complete-checkbox");
+    checkBox.classList.add("task-item__complete");
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task-item__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="task-item__edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="task-item__delete";
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.className = 'delete-icon'
+    deleteButtonImg.className = 'delete__icon'
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -85,14 +85,14 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var editInput=listItem.querySelector('.task-item__input');
+    var label=listItem.querySelector(".task-item__title");
+    var editBtn=listItem.querySelector(".task-item__edit");
+    var containsClass=listItem.classList.contains("task-item_edit-mode");
+    //If class of the parent is .task-item_edit-mode
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .task-item_edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -101,8 +101,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .task-item_edit-mode on the parent.
+    listItem.classList.toggle("task-item_edit-mode");
 };
 
 
@@ -158,9 +158,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var checkBox=taskListItem.querySelector(".task-item__complete");
+    var editButton=taskListItem.querySelector(".task-item__edit");
+    var deleteButton=taskListItem.querySelector(".task-item__delete");
 
 
     //Bind editTask to edit button.
